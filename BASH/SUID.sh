5e5815a6c50db9dbcb11105534d4983011302286
ihack4falafel <hashem.jawad@yahoo.com>
2017-08-21 20:47:38 -0400

    UPDATE - Cleanup

--- BASH/SUID.sh
+++ BASH/SUID.sh
@@ -1,45 +1,44 @@
-echo "##################################"
-echo "#  SUID Files enumeration Script #"
-echo "#       By @ihack4falafel        #"
-echo "##################################"
-echo "                                  "
-echo "                                  "
-echo "[+] Dumping suid files to SUID_FILES.txt..."
-sleep 10
-#Find SUID files and store them in SUID_FILES.txt
-find / \( -perm -4000 \) -exec ls -ld {} \; 2>/dev/null | awk '{print $9}' > SUID_FILES.txt
+echo "#------------------------------------------------#"
+echo "#         SUID Files Enumeration Script          #"
+echo "#------------------------------------------------#"
+echo "                                                 "
 
-echo "[+] Trimming SUID_FILES.txt..."
-sleep 10
+#Find SUID files and store them in SUID_FILES.txt
+echo "[+] Dumping SUID files to SUID_FILES.txt.."
+find / \( -perm -4000 \) -exec ls -ld {} \; 2>/dev/null | awk '{print $9}' > SUID_FILES.txt
+sleep 1
+
 #loop through common linux binaries and then remove them from SUID_FILES.txt
-for bname in '/umount/d' '/su/d' '/mount/d' '/sudo/d' '/passwd/d' '/exim4/d' '/chfn/d' '/chsh/d' '/procmail/d' '/newgrp/d'
+echo "[+] Trimming SUID_FILES.txt..."
+for bname in '/umount/d' '/su/d' '/mount/d' '/sudo/d' '/passwd/d' '/exim4/d' '/chfn/d' '/chsh/d' '/procmail/d' '/newgrp/d' '/ping/d' '/ntfs-3g/d' '/pppd/d' '/pkexec/d' '/ssh-keysign/d'
 do
   sed -i $bname ./SUID_FILES.txt
 done
+sleep 1
 
 echo "[+] Preform strings on the following binaries.."
-echo "                                  "  
-echo "##################################" 
+echo "                                                  "  
+echo "#------------------------------------------------#"
 for line in $(cat SUID_FILES.txt); do                              
-echo "   "$line
+echo " * "$line
 done 
-echo "##################################" 
-echo "                                  " 
-sleep 10
+echo "#------------------------------------------------#"
+echo "                                                  " 
+sleep 5
 
-# string uncommon SUID binaries
+# Perform string command on uncommon SUID binaries
 for line in $(cat SUID_FILES.txt); do 
   echo "             "
-  echo "##################################"
+  echo "#------------------------------------------------#"
   echo $line
-  echo "##################################"
+  echo "#------------------------------------------------#"
   strings $line
-  echo "                                  "
+  echo "                                                  "
   sleep 5
 done
-echo "                                  "
-echo "##################################"
-echo "@@@@@@@Done. happy hunting!@@@@@@@"
-echo "##################################"
+echo "                                                  " 
+echo "#------------------------------------------------#"
+echo "#           Done. Happy hunting!                 #"
+echo "#------------------------------------------------#"
 
 
