b101f3810100846e2afd450092f3a52b462589fb
ihack4falafel <hashem.jawad@yahoo.com>
2017-09-12 19:32:15 -0400

    Command cleanup

--- BASH/SUID.sh
+++ BASH/SUID.sh
@@ -4,17 +4,19 @@ echo "#------------------------------------------------#"
 echo "                                                 "
 
 #Find SUID files and store them in SUID_FILES.txt
-echo "[+] Dumping SUID files to SUID_FILES.txt.."
+echo "[+] Dumping SUID files list into SUID_FILES.txt.."
 find / \( -perm -4000 \) -exec ls -ld {} \; 2>/dev/null | awk '{print $9}' > SUID_FILES.txt
-sleep 1
+sleep 2
 
 #loop through common linux binaries and then remove them from SUID_FILES.txt
-echo "[+] Trimming SUID_FILES.txt..."
-for bname in '/umount/d' '/su/d' '/mount/d' '/sudo/d' '/passwd/d' '/exim4/d' '/chfn/d' '/chsh/d' '/procmail/d' '/newgrp/d' '/ping/d' '/ntfs-3g/d' '/pppd/d' '/pkexec/d' '/ssh-keysign/d'
+echo "[+] Trimming common SUID files from SUID_FILES.txt..."
+
+for bname in '/umount/d' '/su/d' '/mount/d' '/sudo/d' '/passwd/d' '/exim4/d' '/chfn/d' '/chsh/d' '/procmail/d' '/newgrp/d' '/ping/d' '/ntfs-3g/d' '/pppd/d' '/pkexec/d' '/ssh-keysign/d' '/dbus-daemon-launch-helper/d' '/uuidd/d' '/pt_chown/d' '/at/d' '/mtr/d' '/dmcrypt-get-device/d' '/X/d' '/traceroute6.iputils/d' '/polkit-resolve-exe-helper/d' '/polkit-set-default-helper/d' '/polkit-grant-helper-pam/d'
+
 do
   sed -i $bname ./SUID_FILES.txt
 done
-sleep 1
+sleep 2
 
 echo "[+] Preform strings on the following binaries.."
 echo "                                                  "  
