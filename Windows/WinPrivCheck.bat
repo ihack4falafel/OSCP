32814f8a63f14616dff44ab06e3fc1a654664bc7
ihack4falafel <hashem.jawad@yahoo.com>
2017-09-18 13:09:25 -0400

    Add/update

--- Windows/WinPrivCheck.bat
+++ Windows/WinPrivCheck.bat
@@ -102,23 +102,48 @@ type c:\sysprep\sysprep.xml
 type %WINDIR%\Panther\Unattend\Unattended.xml
 type %WINDIR%\Panther\Unattended.xml
 
-rem #-------------------------#
-rem # *.MSI Install privilege #
-rem #-------------------------#
+rem #----------------------------------#
+rem # *.MSI Install - SYSTEM privilege #
+rem #----------------------------------#
 
 @echo off
 rem This will only work if both registry keys contain "AlwaysInstallElevated" with DWORD values of 1.
+rem This setting will allow low privilege user to install any .MSI as system!
 @echo on
 
 reg query HKLM\SOFTWARE\Policies\Microsoft\Windows\Installer\AlwaysInstallElevated 
 reg query HKCU\SOFTWARE\Policies\Microsoft\Windows\Installer\AlwaysInstallElevated
 
-rem #-------------------------------------------#
-rem # RW Service Access For Authenticated Users #
-rem #-------------------------------------------#
+@echo off
+rem  All commands from this point onward will require accesschk.exe
+rem  Make sure you have accesschk.exe(old version) in same directory when you run the script!
+@echo on
+
+rem #---------------------#
+rem # Vulnerable Services #
+rem #---------------------#
 
 @echo off
-rem Make sure you have accesschk.exe(old version) in same directory when you run the script!
+rem  By default WinXP SP1 grant "Authenticated Users" SERVICE_ALL_ACCESS to "SSDPSRV" and "upnphost"!
 @echo on
 
 accesschk.exe /accepteula -uwcqv "Authenticated Users" *
+accesschk.exe /accepteula -uwcqv "Power Users" *
+accesschk.exe /accepteula -uwcqv "Users" *
+
+rem #-------------------------------#
+rem # Vulnerable Folder Permissions #
+rem #-------------------------------#
+
+accesschk.exe /accepteula -uwdqs Users c:\
+accesschk.exe /accepteula -uwdqs "Authenticated Users" c:\
+
+rem #-----------------------------#
+rem # Vulnerable File Permissions #
+rem #-----------------------------#
+
+accesschk.exe /accepteula -uwqs Users c:\*.*
+accesschk.exe /accepteula -uwqs "Authenticated Users" c:\*.*
+
+
+
